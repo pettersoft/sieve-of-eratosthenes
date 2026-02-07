cbf6c5a735f372198b68c0ab82668592bf2b48ed
Petter Pettersson <pettersoft@gmail.com>
2026-02-07 19:34:56 +0100

    remove abstraction

--- turbo.c
+++ turbo.c
@@ -15,7 +15,6 @@
 
 
 const unsigned long int TOTAL_RUNS = 1000000000;
-const int CHAR_SIZE = CHAR_BIT;
 unsigned long int BYTES_REQUIRED = 0;
 
 void calculate_bytes_required();
@@ -31,16 +30,16 @@ int main() {
 	for (int i = 0; (i*2 + 3) * (i * 2 + 3) <= TOTAL_RUNS ; i++) {
 		const int pos = i;
 		const int n = 2*i+3;
-		int arridx = pos / CHAR_SIZE;
-		int bitpos = pos % CHAR_SIZE;
+		int arridx = pos / CHAR_BIT;
+		int bitpos = pos % CHAR_BIT;
 		
 		unsigned char c1 = (arr[arridx] >> bitpos) & 1;
 		if (c1 == 1) {
 			const int start = n*n;
 			for (int j = start; j < TOTAL_RUNS; j+= 2*n) {
 				const int calc = (j-3) / 2;
-				int arridx2 = (calc) / CHAR_SIZE;
-				int bitpos2 = (calc) % CHAR_SIZE;
+				int arridx2 = (calc) / CHAR_BIT;
+				int bitpos2 = (calc) % CHAR_BIT;
 
 
 				arr[arridx2] &= ~(1u << bitpos2);
@@ -52,8 +51,8 @@ int main() {
 	// Kommentera in deh är koden för att skriva ut primtalen
 	/*
 	for (int i = 0; i < TOTAL_RUNS/2; i++) {	
-		int arridx = (i) / CHAR_SIZE;
-		int bitpos = (i) % CHAR_SIZE;
+		int arridx = (i) / CHAR_BIT;
+		int bitpos = (i) % CHAR_BIT;
 		unsigned char c1 = (arr[arridx] >> bitpos) & 1;
 		int n = i*2 + 3;
 		printf("För %d är primtal = %d\n", n, c1);
@@ -67,7 +66,7 @@ int main() {
 
 void calculate_bytes_required() {
 	const double runs = (double)TOTAL_RUNS;
-	const double charsize = (double)CHAR_SIZE;
+	const double charsize = (double)CHAR_BIT;
 
 	BYTES_REQUIRED = ceil(runs / charsize) / 2;
 }

fc5edb89547e7328ad13b4e74b2d4d1b4b149a61
Petter Pettersson <pettersoft@gmail.com>
2026-02-07 19:24:56 +0100

    removed comments

--- turbo.c
+++ turbo.c
@@ -26,11 +26,9 @@ int main() {
 	if (arr == NULL)
 		return 1;
 
-	// set every bit to 1
 	memset(arr, 0XFF, BYTES_REQUIRED);
 
 	for (int i = 0; (i*2 + 3) * (i * 2 + 3) <= TOTAL_RUNS ; i++) {
-		// get which byte we should get
 		const int pos = i;
 		const int n = 2*i+3;
 		int arridx = pos / CHAR_SIZE;
@@ -38,7 +36,6 @@ int main() {
 		
 		unsigned char c1 = (arr[arridx] >> bitpos) & 1;
 		if (c1 == 1) {
-			// mark multiples of this as false
 			const int start = n*n;
 			for (int j = start; j < TOTAL_RUNS; j+= 2*n) {
 				const int calc = (j-3) / 2;
@@ -52,7 +49,7 @@ int main() {
 
 		
 	}
-
+	// Kommentera in deh är koden för att skriva ut primtalen
 	/*
 	for (int i = 0; i < TOTAL_RUNS/2; i++) {	
 		int arridx = (i) / CHAR_SIZE;
@@ -73,7 +70,4 @@ void calculate_bytes_required() {
 	const double charsize = (double)CHAR_SIZE;
 
 	BYTES_REQUIRED = ceil(runs / charsize) / 2;
-
-	// delat med 2 eftersom vi skippar jämna tal
-	
 }
